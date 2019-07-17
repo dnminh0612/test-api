af2570c9c0531272a5629039972b149b08b65e88
NHATMINH <vitop.minhdoan@gmail.com>
2019-07-17 20:38:26 +0700

    up

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['middleware' => ['cors']], function () {
+    Route::get('/minh/test', 'TestController@test')->name('minh.test');
+    Route::get('/minh/test/{user}', 'TestController@show')->name('minh.test.show');
+    Route::get('/products/list', 'ProductsController@index')->name('product.list');
+});
