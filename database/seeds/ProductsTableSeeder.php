6d58ec94a7add495695f210754452167724f8c21
NHATMINH <vitop.minhdoan@gmail.com>
2019-07-18 11:27:30 +0700

    asdsasdasddasasdasd

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -16,16 +16,16 @@ class ProductsTableSeeder extends Seeder
 
         $products = [
             ['Product 1', 'Đây là cái mô tả của sản phẩm', 5 ,100],
-            ['Product 2', 'Đây là cái mô tả của sản phẩm', 6 ,100],
-            ['Product 3', 'Đây là cái mô tả của sản phẩm', 3 ,100],
-            ['Product 4', 'Đây là cái mô tả của sản phẩm', 6 ,100],
-            ['Product 6', 'Đây là cái mô tả của sản phẩm', 2 ,100],
-            ['Product 7', 'Đây là cái mô tả của sản phẩm', 4 ,100],
-            ['Product 8', 'Đây là cái mô tả của sản phẩm', 4 ,100],
-            ['Product 9', 'Đây là cái mô tả của sản phẩm', 4 ,100],
-            ['Product 10', 'Đây là cái mô tả của sản phẩm', 4 ,100],
-            ['Product 11', 'Đây là cái mô tả của sản phẩm', 4 ,100],
-            ['Product 12', 'Đây là cái mô tả của sản phẩm', 4 ,100],
+            ['Product 2', 'Đây là cái mô tả của sản phẩm', 6 ,200],
+            ['Product 3', 'Đây là cái mô tả của sản phẩm', 3 ,300],
+            ['Product 4', 'Đây là cái mô tả của sản phẩm', 6 ,500],
+            ['Product 6', 'Đây là cái mô tả của sản phẩm', 2 ,2200],
+            ['Product 7', 'Đây là cái mô tả của sản phẩm', 4 ,300],
+            ['Product 8', 'Đây là cái mô tả của sản phẩm', 4 ,500],
+            ['Product 9', 'Đây là cái mô tả của sản phẩm', 4 ,700],
+            ['Product 10', 'Đây là cái mô tả của sản phẩm', 4 ,1000],
+            ['Product 11', 'Đây là cái mô tả của sản phẩm', 4 ,2000],
+            ['Product 12', 'Đây là cái mô tả của sản phẩm', 4 ,300],
         ];
 
         foreach ($products as $user) {
