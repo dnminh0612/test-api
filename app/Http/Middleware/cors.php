6e3eae122d695621eb1bad1ea6fb204e0f1637e2
NHAT MINH <vitop.minhdoan@gmail.com>
2019-07-28 00:00:25 -0700

    asdasd

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -17,6 +17,6 @@ class cors
     {
         return $next($request)->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
