695632527d85539cf20356758875b6ee6e417aa1
NHAT MINH <vitop.minhdoan@gmail.com>
2019-09-22 05:27:50 +0700

    123

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
+
+class ProductsController extends Controller
+{
+    private static $DEFAULT_LIMIT_RECORD_PER_PAGE = 10;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $page = request('page') ?: 1;
+        $limit = request('limit') ?: self::$DEFAULT_LIMIT_RECORD_PER_PAGE;
+
+        Paginator::currentPageResolver(function () use ($page) {
+            return $page;
+        });
+
+        $result = Product::orderBy('id', 'desc')->paginate($limit);
+
+        return response()->json($result);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $product =  new Product([
+            'name' => $data[0]->productName,
+            'description' => $data[0]->description,
+            'quantity' => $data[0]->quantity,
+            'price' => $data[0]->price,
+        ]);
+        $product->save();
+        return response()->json('ngon');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
